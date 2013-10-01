8b1bbbc500b92dee60ca8613e1c8940b78a73edb
drbaumgartne <drbaumgartne@gmail.com>
2013-10-01 16:58:16 -0700

    updated todo.hs with the basic add and delete options

--- todo.hs
+++ todo.hs
@@ -5,6 +5,7 @@
 
 -- imports
 import System.IO
+import System.Directory
 
 -- thought process for building (at least according to LYAHFGG)
 -- BASICS -> COMPLEX 
@@ -19,11 +20,54 @@ import System.IO
 --      Take input
 --      Append to file
 
+-- RAW DELETE:
+--      Ask for add or delete
+--      Display items currently on the list
+--      Ask for which deletion
+--      Write new file with handles 
+--
 
 -- main
 main = do
-    putStrLn "Add to to-do list:"
-    todoItems <- getLine
-    appendFile "todo.txt" (todoItems ++ "\n") -- add a new line for spacing
+    putStrLn "Add or Delete (A/D):"
+    response <- getLine
+    if qAddDelete response
+        then do putStrLn "Add to to-do list:"
+                todoItems <- getLine
+                appendFile "todo.txt" (todoItems ++ "\n") -- new line for spacing
+        -- read file, ask about task to delete, remove it, create a temp file, delete orig, rename temp
+        else do contents <- readFile "todo.txt"
+                putStrLn "Which task has been completed:"
+                mapM_ putStrLn $ enumList contents
+                deletion <- getLine
+                let number = read deletion
+                    newtodoItems = unlines $ removeItem (number - 1) (lines contents)
+                (tempFile, tempHandle) <- openTempFile "." "temp"
+                hPutStr tempHandle newtodoItems
+                hClose tempHandle
+                removeFile "todo.txt"
+                renameFile tempFile "todo.txt"
 
--- help functions
+
+-- helper functions
+
+-- qAddDelete :: Bool check for response
+qAddDelete :: String -> Bool
+qAddDelete c
+    | c == "A"  = True
+    -- todo: c == "D" = False
+    -- todo: otherwise = error handling somehow
+    | otherwise = False
+
+-- enumList :: convert the read file into lines and add contextual information
+enumList :: String -> [String]
+enumList contents = zipWith (\n line -> show n ++ " - " ++ line) [1..] (lines contents)
+
+-- removeItem :: removes the n-th item given
+removeItem :: Int -> [String] -> [String]
+removeItem n xs = let (ys,zs) = splitAt n xs in ys ++ (tail zs)
+-- if we had Data.List there is a delete command (which deletes the first occurence of something)
+-- removeItem :: delete ((lines contents) !! (number - 1)) (line contents)
+-- more hardcore would be
+-- removeItem eqx (y:ys) = if eqx == y then ys else y : removeItem eqx ys
+-- in the above you would need to provide the item to match (eqx "equitable x item")
